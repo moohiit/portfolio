5aff103a22205369808748c80560f13024da8f1f
Mohit Patel <pmohit645@gmail.com>
2024-08-27 17:40:39 +0530

    Added mail functionality for get in touch section

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# portfolio-
+# Mohit Patel's Portfolio
+
+A showcase of projects, skills, and experience in web development and software engineering.
+
+## Features
+- Responsive design
+- Project demos and descriptions
+- Interactive contact form with email functionality
+
+## Demo
+#### Homepage
+![Homepage](./images/portfolio.png)
+#### Projects Section
+![Projects Section](./images/Project_section.png)
+
+## Table of Contents
+- [Installation](#installation)
+- [Technologies](#technologies)
+- [Projects](#projects)
+- [License](#license)
+- [Contact](#contact)
+
+## Installation
+1. Clone the repo: `git clone https://github.com/moohiit/portfolio.git`
+2. Install dependencies: `npm install`
+3. Run the project: `npm start`
+
+## Technologies
+- **Frontend:** React, Tailwind CSS, Material UI
+- **Backend:** Node.js, Express.js, PHP, MongoDB
+
+## Projects
+### Watchman System
+A security management system at colleges...
+### Social Media and Messaging Website
+A full-stack application...
+
+## License
+This project is licensed under the MIT License.
+
+## Contact
+- GitHub: [moohiit](https://github.com/moohiit)
+- Email: mohit.patel.edu@gmail.com
+- Linkedin: [Mohit Patel](https://www.linkedin.com/in/mohit-patel-51338a245/)
--- js/script.js
+++ js/script.js
@@ -1,53 +1,57 @@
 const hamburger = document.querySelector(".hamburger");
-        const navLinks = document.querySelector(".nav-links");
-        const links = document.querySelectorAll(".nav-links li");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-        hamburger.addEventListener('click', () => {
-            // Animate Links
-            navLinks.classList.toggle("open");
-            links.forEach(link => {
-                link.classList.toggle("fade");
-            });
+hamburger.addEventListener("click", () => {
+  // Animate Links
+  navLinks.classList.toggle("open");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
 
-            // Hamburger Animation
-            hamburger.classList.toggle("toggle");
-        });
+  // Hamburger Animation
+  hamburger.classList.toggle("toggle");
+});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    var options = {
-        strings: ["Web Developer", "Full Stack Developer"],
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 1000,
-        loop: true
-    };
-    var typed = new Typed(".home-content h3 span", options);
+document.addEventListener("DOMContentLoaded", function () {
+  var options = {
+    strings: ["Web Developer", "Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true,
+  };
+  var typed = new Typed(".home-content h3 span", options);
 });
 
 let slideIndex = 1;
-    let slideInterval = setInterval(() => plusSlides(1), 3000); // Change slide every 3 seconds
+let slideInterval = setInterval(() => plusSlides(1), 3000); // Change slide every 3 seconds
 
-    function plusSlides(n) {
-        showSlides(slideIndex += n);
-    }
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-    function showSlides(n) {
-        let i;
-        const slides = document.querySelectorAll('.slide');
-        const dots = document.querySelectorAll('.dot');
-        if (n > slides.length) {slideIndex = 1}
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(' active', '');
-        }
-        slides[slideIndex-1].style.display = 'block';
-        dots[slideIndex-1].className += ' active';
-    }
+function showSlides(n) {
+  let i;
+  const slides = document.querySelectorAll(".slide");
+  const dots = document.querySelectorAll(".dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
